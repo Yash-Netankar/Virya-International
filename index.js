bed629a18019157cb443b620311660d24b243a16
Yash-Netankar <netankaryash2002@gmail.com>
2021-07-04 17:58:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const navbar = document.querySelector(".my_navbar");
+const hamburger = document.getElementById("my_hamburger");
+const sidebar = document.getElementById("my_sidebar");
+const slidebar_close_btn = document.querySelector(".sidebar_close_btn");
+
+// making navbar sticky when page is scrolled
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 15) {
+        navbar.classList.add("sticky");
+    }
+    else {
+        navbar.classList.remove("sticky");
+    }
+});
+
+
+
+// toggling of sidebar functionality
+hamburger.addEventListener("click", () => {
+    sidebar.classList.toggle("show");
+});
+slidebar_close_btn.addEventListener("click", () => {
+    sidebar.classList.remove("show");
+});
