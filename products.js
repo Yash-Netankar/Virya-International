683f9aa68bf7e3fc44a71489c0f9415f5bf2ba51
Yash-Netankar <netankaryash2002@gmail.com>
2021-07-09 22:23:55 +0530

    added a new product page working

--- products.js
+++ products.js
@@ -47,25 +47,35 @@ const imgaddr = "./styles/images/products/";
 const product_cont = document.getElementById("products_container");
 const products_section = document.querySelector(".products_section");
 
+const replace_trailing_commas = /['"`,]+/g;
 
 
-
+// append data to html
 unique_categories.map(category => {
     let str = `
     <div class='product_category'>
-    <h1>${category}</h1>
+    <h1 class='cat_name'>${category}</h1>
     <div class="all_products">
     ${data.map(product => {
         if (product.cat === category) {
             return (`
                 <div class='product_card'>
-                <img src=${imgaddr + product.product + '.jpg'} alt=${product.product}>
-                <span class='name'>${product.product}</span>
-            </div>`
+                <img class='product_img' src=${imgaddr + product.product + '.jpg'} alt=${product.product}>
+                <span class='name'>${product.product.toUpperCase()}</span>
+                </div>`
             )
         }
     })}
     </div>
     </div>`
-    products_section.insertAdjacentHTML("beforeend", str);
+    products_section.insertAdjacentHTML("beforeend", str.replace(replace_trailing_commas, ""));
+});
+
+const product_images = document.querySelectorAll(".product_img");
+
+// logic for product image to apply when click\
+product_images.forEach(img => {
+    img.addEventListener("click", (e) => {
+        product_cont.style.backgroundImage = `url(${img.src})`;
+    })
 })
